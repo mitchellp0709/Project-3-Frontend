f60f6459f31a5c05b97a1f2ba4cb32d42ef15a61
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-11-19 16:51:13 -0500

    made profile page, fixed header

--- src/components/FirstHeader.js
+++ src/components/FirstHeader.js
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 
 const FirstHeader = (props) => {
   return (
-    <nav>
+    <nav className="first-header">
       <Link to="/signup">
         <h2>Signup</h2>
       </Link>
+      <h1 className="retweet-main">Retweet</h1>
       <Link to="/login">
         <h2>Login</h2>
       </Link>

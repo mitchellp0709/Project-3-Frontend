afaedebb61682f4270343277f7c122ec6f6f8917
marcod0410 <dlsmarco0410@gmail.com>
2021-11-19 17:27:15 -0500

    working on show page

---
+++ src/pages/show.js
@@ -0,0 +1,29 @@
+import { useParams, useNavigate } from "react-router-dom";
+import {useState, useEffect} from "react"
+
+const Show = (props) => {
+  
+  const navigate = useNavigate()
+
+  const params = useParams();
+
+  const id = params.id;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default Show;

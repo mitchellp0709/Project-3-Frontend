c02b492442b5a2f505f868b2e7cc691ac1015ee4
Donovan Gallaway <donovangallaway@gmail.com>
2021-11-19 08:55:29 -0800

    added username state and localStorage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react'
 import Header from './components/Header'
 import Signup from './pages/signup'
 import Login from './pages/login'
 import {Route, Routes, Link,} from 'react-router-dom'
+import {useState} from 'react'
 import "./styles.scss"
 
 
@@ -11,7 +11,8 @@ import "./styles.scss"
 
 function App() {
 
-  const [token, setToken] = React.useState({})
+  const [token, setToken] = useState({})
+  const [username, setUsername] = ({})
 
 const URL = "https://group-3-project-3.herokuapp.com/"
 
@@ -20,6 +21,11 @@ const getToken = async (token) => {
 
   setToken(token)
   localStorage.setItem("token", JSON.stringify(token))
+}
+
+const getUsername = async (username) => {
+  setUsername({username: username})
+  localStorage.setItem('username', username)
 }
 
   return (
@@ -32,7 +38,7 @@ const getToken = async (token) => {
           <Routes>
           <Route exact path="/" element={<h1>Home</h1>}/>
           <Route path="/signup" element={<Signup URL={URL}/>}/>
-          <Route path="/login" element={<Login URL={URL} getToken={getToken}/>}/>
+          <Route path="/login" element={<Login URL={URL} getToken={getToken} getUsername={getUsername}/>}/>
           </Routes>
         
 

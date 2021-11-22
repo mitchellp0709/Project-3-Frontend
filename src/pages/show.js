7fcbd8f38647ca1f2ffe9f8d6dee14fdc35ce503
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-11-21 19:24:16 -0500

    working delete button

--- src/pages/show.js
+++ src/pages/show.js
@@ -1,60 +1,79 @@
 import { useParams, useNavigate } from "react-router-dom";
-import {useState, useEffect} from "react"
+// import {useState, useEffect} from "react"
 
-const Show = (props) => {
+// const Show = (props) => {
 
-  const navigate = useNavigate()
+//   const navigate = useNavigate()
   
-  const params = useParams();
+//   const params = useParams();
  
-  const id = params.id;
+//   const id = params.id;
   
-  const tweets = props.tweets;
+//   const tweets = props.tweets;
  
-  const [editForm, setEditForm] = useState({})
+//   const [editForm, setEditForm] = useState({})
  
-  useEffect(() => {
-      if(props.tweets){
-          const Tweet = tweets.find((t) => t._id === id);
-          setEditForm(oneCheese)
-      }
-  }, [props.tweets])
+//   useEffect(() => {
+//       if(props.tweets){
+//           const Tweet = tweets.find((t) => t._id === id);
+//           setEditForm(oneCheese)
+//       }
+//   }, [props.tweets])
 
-  if (props.tweets) {
+//   if (props.tweets) {
   
-    const oneTweet = tweets.find((t) => t._id === id);
+//     const oneTweet = tweets.find((t) => t._id === id);
     
 
-    const handleChange = (event) => {
-        // create a copy of the state
-        const newState = {...editForm}
-        // update the newState
-        newState[event.target.name] = event.target.value
-        // update the state
-        setEditForm(newState)
-    }
+//     const handleChange = (event) => {
+//         // create a copy of the state
+//         const newState = {...editForm}
+//         // update the newState
+//         newState[event.target.name] = event.target.value
+//         // update the state
+//         setEditForm(newState)
+//     }
 
-      // handleSubmit for form
-      const handleSubmit = (event) => {
-        // prevent the refresh
-        event.preventDefault()
-        // pass the form data to updateCheese
-        props.updateCheese(editForm, Tweet._id)
+//       // handleSubmit for form
+//       const handleSubmit = (event) => {
+//         // prevent the refresh
+//         event.preventDefault()
+//         // pass the form data to updateCheese
+//         props.updateCheese(editForm, Tweet._id)
    
-        navigate("/")
-    }
+//         navigate("/")
+//     }
    
 
  
 
-    return (
-      <div className="tweet">
+//     return (
+//       <div className="tweet">
         
-      </div>
-    );
-  } else {
-    return <h1>Show page</h1>;
-  }
-};
+//       </div>
+//     );
+//   } else {
+//     return <h1>Show page</h1>;
+//   }
+// };
 
-export default Show;
+// export default Show;
+
+
+
+const Show = (props) => {
+  const params = useParams();
+  const id = params.id;
+  console.log(id)
+
+  const getTweet = async()=> {
+    const response = await fetch(props.url + id)
+    const data = await response.json()
+    return data
+  }
+  
+
+  return <div>show </div>;
+}
+
+export default Show

744fda37f7098eeb971f16c5be09b8b0a701d823
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-11-21 21:21:36 -0500

    cover photos and bprofile photos added with defaults

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -2,22 +2,47 @@ import Header from "../components/Header";
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Comment from "../components/comments";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+
 
 const Profile = (props) => {
+  const params=useParams()
+  const username = params.id;
+  const [profPic, setProfPic] = useState(null);
+  const [coverPic, setCoverPic] = useState(null);
   
+
+  const getProfile = async() => {
+    const response = await fetch(`${props.URL}auth/user/${username}`)
+    const data = await response.json()
+    setProfPic(data[0].profilePic);
+    setCoverPic(data[0].coverPic);
+    
+  }
+ 
+  useEffect(() => { getProfile() }, [])
+  if (profPic === undefined) {
+    setProfPic(
+      "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+    );
+  }
+  if (coverPic === undefined) {
+     setCoverPic(
+       "https://i.pinimg.com/474x/50/b1/a8/50b1a89e3201d7adec2cb16f0660b2fe--photo-ideas.jpg"
+     );
+   }
 
  
   return (
     <>
       <Header />
-      <div className="cover-photo">
+      <div className="cover-photo" style={{backgroundImage:`url(${coverPic})`}}>
         <img
           className="profile-picture"
-          src="https://imgix.ranker.com/list_img_v2/19241/2779241/original/best-frank-reynolds-quotes?w=817&h=427&fm=jpg&q=50&fit=crop"
+          src={profPic}
           alt="profile picture"
         />
-        <h2 className="profile-user-name">$Sample User Name</h2>
+        <h2 className="profile-user-name">${username}</h2>
       </div>
     </>
   );
